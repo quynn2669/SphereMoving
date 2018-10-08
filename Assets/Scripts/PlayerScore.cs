46cdda1523356632b6bb07bfcb234e0cdcfe1859
Quy Nguyen Ngoc <quychuot@gmail.com>
2018-10-08 10:37:53 +0700

    Add files via upload

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour {
+
+	public Transform player;
+	public Text scoreText;
+	void Update () {
+		scoreText.text = player.position.z.ToString("0");
+	}
+}
